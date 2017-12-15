af0c85a33618ce6f28ce8df440fd9195434bf86f
Tomasz Frankiewicz <tomaszfrankiewicz@Tomaszs-Mac.local>
2017-12-15 02:14:52 -0800

    init

---
+++ lab2/lab2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab2
+//
+//  Created by Tomasz Frankiewicz on 15/12/2017.
+//  Copyright © 2017 Tomasz Frankiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
